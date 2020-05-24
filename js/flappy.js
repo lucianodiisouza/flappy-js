11d9399f2a0741e781209cabb4d6512f13aad95e
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-05-24 20:40:08 -0300

    feature: collisions and last adjustments

--- js/flappy.js
+++ js/flappy.js
@@ -108,6 +108,33 @@ function Progresso() {
   this.atualizarPontos(0)
 }
 
+function estaoSobrepostos(elementoA, elementoB) {
+  const a = elementoA.getBoundingClientRect()
+  const b = elementoB.getBoundingClientRect()
+
+  const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left
+  const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top
+
+  return horizontal && vertical
+}
+
+function colidiu(passaro, barreiras) {
+  let colidiu = false
+
+  barreiras.pares.forEach((parDeBarreiras) => {
+    if (!colidiu) {
+      const superior = parDeBarreiras.superior.elemento
+      const inferior = parDeBarreiras.inferior.elemento
+
+      colidiu =
+        estaoSobrepostos(passaro.elemento, superior) ||
+        estaoSobrepostos(passaro.elemento, inferior)
+    }
+  })
+
+  return colidiu
+}
+
 function Flappybird() {
   let pontos = 0
 
@@ -131,6 +158,10 @@ function Flappybird() {
     const temporizador = setInterval(() => {
       barreiras.animar()
       passaro.animar()
+
+      if (colidiu(passaro, barreiras)) {
+        clearInterval(temporizador)
+      }
     }, 20)
   }
 }
